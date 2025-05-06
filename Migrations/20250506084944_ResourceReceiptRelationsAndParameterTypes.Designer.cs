1e9ea8231ff76b496ee3fa805cb656bce7dbc283
Yoga Subagja <42853517+Sality32@users.noreply.github.com>
2025-05-06 15:50:14 +0700

    create migration file

--- Migrations/20250506084944_ResourceReceiptRelationsAndParameterTypes.Designer.cs
+++ Migrations/20250506084944_ResourceReceiptRelationsAndParameterTypes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ReceiptInformationSystem.Infrastructure.Data;
@@ -11,9 +12,11 @@ using ReceiptInformationSystem.Infrastructure.Data;
 namespace ReceiptInformationSystem.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250506084944_ResourceReceiptRelationsAndParameterTypes")]
+    partial class ResourceReceiptRelationsAndParameterTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250506084944_ResourceReceiptRelationsAndParameterTypes.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReceiptInformationSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResourceReceiptRelationsAndParameterTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ParameterTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParameterTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StepReceipts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ReceiptId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StepReceipts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StepReceipts_Receipts_ReceiptId",
+                        column: x => x.ReceiptId,
+                        principalTable: "Receipts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ParameterSteps",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false),
+                    StepReceiptId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParameterSteps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ParameterSteps_StepReceipts_StepReceiptId",
+                        column: x => x.StepReceiptId,
+                        principalTable: "StepReceipts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParameterSteps_StepReceiptId",
+                table: "ParameterSteps",
+                column: "StepReceiptId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StepReceipts_ReceiptId",
+                table: "StepReceipts",
+                column: "ReceiptId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParameterSteps");
+
+            migrationBuilder.DropTable(
+                name: "ParameterTypes");
+
+            migrationBuilder.DropTable(
+                name: "StepReceipts");
+        }
+    }
+}
